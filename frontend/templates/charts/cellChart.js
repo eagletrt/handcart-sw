e4ccb1a23e7364c3b977457a5dd6b90363cb62ef
Lorenzo <lorenzo.rossi-4@studenti.unitn.it>
2021-03-07 19:56:25 +0100

    cell and battery charts

---
+++ frontend/templates/charts/cellChart.js
@@ -0,0 +1,81 @@
+am4core.ready(function() {
+
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+
+    // Create chart instance
+    var chart = am4core.create("cellChart", am4charts.XYChart);
+    chart.scrollbarX = new am4core.Scrollbar();
+
+    // Add data
+    chart.data = [];
+    ncells = 100;
+
+    for (i = 0; i < ncells; i++) {
+        battery = Math.floor(Math.random() * 101);
+        cellName = "CELL " + (i + 1);
+
+        element = {
+            "cell": cellName,
+            "battery": battery
+        };
+        chart.data.push(element);
+    }
+
+    // Create axes
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "cell";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 30;
+    categoryAxis.renderer.labels.template.horizontalCenter = "right";
+    categoryAxis.renderer.labels.template.verticalCenter = "middle";
+    categoryAxis.renderer.labels.template.rotation = 270;
+    categoryAxis.tooltip.disabled = true;
+    categoryAxis.renderer.minHeight = 110;
+
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.renderer.minWidth = 50;
+
+    // Create series
+    var series = chart.series.push(new am4charts.ColumnSeries());
+    series.sequencedInterpolation = true;
+    series.dataFields.valueY = "battery";
+    series.dataFields.categoryX = "cell";
+    series.tooltipText = "[{categoryX}: bold]{valueY}[/]";
+    series.columns.template.strokeWidth = 0;
+
+    series.tooltip.pointerOrientation = "vertical";
+
+    series.columns.template.column.cornerRadiusTopLeft = 10;
+    series.columns.template.column.cornerRadiusTopRight = 10;
+    series.columns.template.column.fillOpacity = 0.8;
+
+    // on hover, make corner radiuses bigger
+    var hoverState = series.columns.template.column.states.create("hover");
+    hoverState.properties.cornerRadiusTopLeft = 0;
+    hoverState.properties.cornerRadiusTopRight = 0;
+    hoverState.properties.fillOpacity = 1;
+
+    series.columns.template.adapter.add("fill", function(fill, target) {
+        var i = target.dataItem.index;
+        var perc = chart.data[i].battery;
+
+        if (perc < 50) {
+            r = 255;
+            g = Math.round(5.1 * perc);
+        } else {
+            g = 255;
+            r = Math.round(510 - 5.10 * perc);
+        }
+        b = 0;
+
+        rgb = `rgb(${r}, ${g}, ${b})`;
+        return rgb;
+        /*return chart.colors.getIndex(target.dataItem.index);*/
+    });
+
+    // Cursor
+    chart.cursor = new am4charts.XYCursor();
+
+}); // end am4core.ready()
---
+++ frontend/templates/charts/chargeChart.js
@@ -0,0 +1,85 @@
+am4core.ready(function() {
+
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+
+    // create chart
+    var chart = am4core.create("chargeChart", am4charts.GaugeChart);
+    chart.innerRadius = am4core.percent(70);
+    chart.startAngle = 130;
+    chart.endAngle = 410;
+
+    /**
+     * Normal axis
+     */
+
+    var axis = chart.xAxes.push(new am4charts.ValueAxis());
+    axis.min = 0;
+    axis.max = 100;
+    axis.strictMinMax = false;
+    axis.renderer.radius = am4core.percent(0);
+
+    /**
+     * Axis for ranges
+     */
+
+    var axis2 = chart.xAxes.push(new am4charts.ValueAxis());
+    axis2.min = 0;
+    axis2.max = 100;
+    axis2.strictMinMax = true;
+    axis2.renderer.labels.template.disabled = true;
+    axis2.renderer.ticks.template.disabled = true;
+    axis2.renderer.grid.template.disabled = true;
+
+    var range0 = axis2.axisRanges.create();
+    range0.value = 0;
+    range0.endValue = 50;
+    range0.axisFill.fillOpacity = 1;
+    range0.axisFill.fill = am4core.color("green");
+
+    var range1 = axis2.axisRanges.create();
+    range1.value = 50;
+    range1.endValue = 100;
+    range1.axisFill.fillOpacity = 1;
+    range1.axisFill.fill = am4core.color("red");;
+
+    /**
+     * Label
+     */
+
+    var label = chart.radarContainer.createChild(am4core.Label);
+    label.isMeasured = false;
+    label.fontSize = 45;
+    label.x = am4core.percent(50);
+    label.y = am4core.percent(100);
+    label.horizontalCenter = "middle";
+    label.verticalCenter = "middle";
+    label.text = "50%";
+
+
+    /**
+     * Hand
+     */
+
+    var hand = chart.hands.push(new am4charts.ClockHand());
+    hand.axis = axis2;
+    hand.value = 50;
+    hand.visible = false;
+
+    hand.events.on("propertychanged", function(ev) {
+        range0.endValue = ev.target.value;
+        range1.value = ev.target.value;
+        label.text = axis2.positionToValue(hand.currentPosition).toFixed(1) + "%";
+        axis2.invalidate();
+    });
+
+    setInterval(function() {
+        var value = Math.round(Math.random() * 100);
+        var animation = new am4core.Animation(hand, {
+            property: "value",
+            to: value
+        }, 1000, am4core.ease.cubicOut).start();
+    }, 2000);
+
+}); // end am4core.ready()
