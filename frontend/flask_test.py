3bf57827ed0550627f22ffb7c3409acf6aed5abb
Lorenzo <lorenzo.rossi-4@studenti.unitn.it>
2021-03-11 14:15:21 +0100

    adapted all files to flask

--- frontend/flask_test.py
+++ frontend/flask_test.py
@@ -1,14 +1,22 @@
-import flask
+from flask import Flask, render_template, url_for
 from flask import request
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
+# pages
 
 @app.route('/', methods=['GET'])
 def home():
-    return flask.render_template("index.html")
+    return render_template("index.html")
 
+@app.route('/warning')
+def warning():
+    return render_template("warning.html")
+
+@app.route('/error')
+def error():
+    return render_template("error.html")
 
 @app.route('/command/', methods=['POST'])
 def recv_command():
@@ -20,4 +28,14 @@ def send_status():
     return "{\"timestamp\": \"2020-12-01:ora\", \"state\": \"IDLE\"}"
 
 
-app.run()
+@app.route('/handcart/errors/', methods=['GET'])
+def send_errors():
+    return "{\"timestamp\": \"2020-12-01:ora\", \"error code\": \"ERRORCODE01\"}"
+
+
+@app.route('/handcart/warnings/', methods=['GET'])
+def send_warnings():
+    return "{\"timestamp\": \"2020-12-01:ora\", \"state\": \"WARNINGCODE01\"}"
+
+if __name__ == '__main__':
+    app.run()
