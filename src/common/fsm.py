2ac10ee81f930e571d33d2d11a86b08b6821a696
Antonio Gelain <antonio.gelain2@gmail.com>
2026-09-12 15:41:17 +0200

    fix: change is_enabled function to is_charging

--- src/common/fsm.py
+++ src/common/fsm.py
@@ -215,7 +215,7 @@ class FSM(threading.Thread):
         GPIO.output(PIN.PON_CONTROL.value, GPIO.HIGH)  # Enable PON
 
         # Check that charger is not charging
-        if self.canread.charger.is_enabled():
+        if self.canread.charger.is_charging():
             self.canread.can_charger_charge_enabled = False
 
         if self.balancing_command:
@@ -239,7 +239,7 @@ class FSM(threading.Thread):
         self.precharge_asked_time = 0
 
         # Check that charger is not charging
-        if self.canread.charger.is_enabled():
+        if self.canread.charger.is_charging():
             self.canread.can_charger_charge_enabled = False
 
         # Make sure that acc is in ts off
@@ -265,7 +265,7 @@ class FSM(threading.Thread):
         GPIO.output(PIN.DISCHARGE.value, GPIO.HIGH)  # pin discharge high (open discharge relay)
 
         # Check that charger is not charging
-        if self.canread.charger.is_enabled():
+        if self.canread.charger.is_charging():
             self.canread.can_charger_charge_enabled = False
             return STATE.IDLE
 
@@ -302,7 +302,7 @@ class FSM(threading.Thread):
         """
 
         # Check that charger is not charging
-        if self.canread.charger.is_enabled():
+        if self.canread.charger.is_charging():
             self.canread.can_charger_charge_enabled = False
 
         if self.canread.bms_hv.status != HvStatus.TS_ON:
@@ -353,7 +353,7 @@ class FSM(threading.Thread):
         """
 
         # Check that charger is not charging
-        if self.canread.charger.is_enabled():
+        if self.canread.charger.is_charging():
             self.canread.can_charger_charge_enabled = False
 
         if self.canread.bms_hv.status != HvStatus.TS_ON:
@@ -373,7 +373,7 @@ class FSM(threading.Thread):
             return STATE.IDLE
 
         # Check that charger is not charging
-        if self.canread.charger.is_enabled():
+        if self.canread.charger.is_charging():
             self.canread.can_charger_charge_enabled = False
 
         if not self.canread.bms_hv.is_balancing == Toggle.ON \
